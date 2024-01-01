c4f27c451c219eb2a028a9276c4746952fa300ee
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-01-01 09:09:27 +0600

    sumOfArray

---
+++ Loops/sumOfArray.js
@@ -0,0 +1,12 @@
+// sum of all natural numbers 1 to n
+
+function sumOfArray(num){
+    // let sum=0;
+    // for(i = 1; i <= num; i++){
+    //     sum += i;
+    // }
+
+    return num*(num+1)/2;
+}
+
+console.log(sumOfArray(10))
