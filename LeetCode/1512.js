7ded7425b610bf165ebfb09b8c1d2724917b50b8
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-01-04 12:04:47 +0600

    1512

--- LeetCode/1512.js
+++ LeetCode/1512.js
@@ -1,5 +1,13 @@
 var numIdenticalPairs = function(nums) {
-    
+    let goodPair = 0;
+    for(let i=0; i<nums.length; i++){
+        for(let j=i+1; j<nums.length; j++){
+            if(nums[i]===nums[j]){
+                goodPair++;
+            }
+        }
+    }
+    return goodPair; 
 };
 
-console.log(numIdenticalPairs([12,23,34,11]))
+console.log(numIdenticalPairs([1,1,1,1]))
