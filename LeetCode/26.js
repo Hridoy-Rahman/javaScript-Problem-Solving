7b42d1066bb32104503486c61a404fffbf5427d7
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-01-11 16:11:11 +0600

    26

---
+++ LeetCode/26.js
@@ -0,0 +1,8 @@
+var removeDuplicates = function(nums) {
+    for(let i=0; i<nums.length - 1; i++){
+        if(nums[i] === nums[i+1]){
+            nums.splice(i+1, 1);
+            i--
+        }
+    }
+};
