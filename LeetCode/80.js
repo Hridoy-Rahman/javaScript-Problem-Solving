7c317eac5145a9c698013eca3e87d0bf4a5d8c65
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-01-11 16:26:12 +0600

    80

---
+++ LeetCode/80.js
@@ -0,0 +1,12 @@
+var removeDuplicates = function(nums) {
+    let j=0;
+
+    for(let i=0; i < nums.length; i++){
+        if(nums[i] !== nums[i+2]){
+            nums[j] = nums[i];
+            j++
+        }
+    }
+
+    return j;
+};
