d239feff8c879d7f0819b17c222e9dd9f9a662a2
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-01-13 09:25:07 +0600

    179

---
+++ LeetCode/179.js
@@ -0,0 +1,10 @@
+var largestNumber = function(nums) {
+    if(!nums || nums.length === 0){
+        return '0';
+    }
+    nums.sort((a,b)=>`${b}${a}` - `${a}${b}`);
+    if(nums[0] === 0){
+        return '0';
+    }
+    return nums.join('');
+};
