b62f2ac131697e51aee93f6f2a7c012600b79bcd
Hridoy-Rahman <hridoyrahman715@gmail.com>
2024-11-19 08:57:15 +0600

    two sum optimized solution

--- LeetCode/1.js
+++ LeetCode/1.js
@@ -1,11 +1,34 @@
-var twoSum = function(nums, target) {
-    let sum=0;
-    for(let i = 0; i<nums.length; i++){
-        for(let j = i + 1; j<nums.length; j++){
-            if(nums[i] + nums[j]===target){
-                return [i,j]
-            }
-        }
-    }
 
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    // Bruteforce  O(n^2)
+   // for(let i=0; i<nums.length; i++){
+   //     for(let j= i+1; j<nums.length; j++){
+   //         if( nums[i] + nums[j]=== target )
+   //             return [i,j]
+   //     }
+   // }
+    
+    
+    // using hasmap Time complexity O(n)
+    const map = new Map()
+    
+    for(let i=0; i<nums.length; i++){
+        complement = target - nums[i];
+        
+        if(map.has(complement)){
+            return [map.get(complement),i]
+        }
+        
+        map.set(nums[i],i)
+    }
+    
+    return [];
 };
+
