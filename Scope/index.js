87a925d42f3aa050883bf6e590d9ffea08b99b33
Hridoy-Rahman <hridoyrahman715@gmail.com>
2025-02-17 20:17:51 +0600

    hgsdsghd

---
+++ Scope/index.js
@@ -0,0 +1,20 @@
+//Global scope
+// let name = "Hridoy"                 
+
+// function greet(){
+//     console.log("Hello " + name)
+// }
+
+// console.log(name)               //output: Hridoy
+
+// greet()                         // Output : Hello Hriody
+
+
+//Function scope
+// function test(){
+//     let x= 10
+//     console.log(x)
+// }
+
+// test()                      // Output : 10
+// console.log(x)              //Output : undefined
---
+++ function/index.js
@@ -0,0 +1,15 @@
+// function add( a , b){
+//     return a + b
+// }
+// console.log(add(2,3))
+
+// function multiply( a , b){
+//     return a * b
+// }
+// console.log(multiply(2,3))
+
+// const divide = (a,b)=> a/b          //arrow function
+// console.log(divide(2,3))
+
+// const square = (num) => num*num
+// console.log(square(0.5))
